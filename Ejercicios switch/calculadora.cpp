e146c6c0ea163c801ee448a043584b2301f2969c
dliezer <00203725@uca.edu.sv>
2025-06-22 20:33:32 -0600

    .

---
+++ Ejercicios switch/calculadora.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int op, num1, num2;
+    cout << "Bienvenido/a a la calculadora, por favor, ingrese una opción:\n";
+    cout << "1. Suma\n";
+    cout << "2. Resta\n";
+    cout << "3. Multiplicacion\n";
+    cout << "4. Division\n";
+    cout << "5. Exponente\n;
+    cout << "6. Modulo\n";
+    cout << "Salir\n";
+    cin >> op;
+
+    switch (op) {
+      case 1:
+        cout << "Ingresa los 2 numeros para hacer la suma\n";
+        cin >> num1, num2;
+}
+}
